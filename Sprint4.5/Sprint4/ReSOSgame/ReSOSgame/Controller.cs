e07e1c1db213578d193c5a48203a11458bbc5e01
GabrielBarrientos99 <gabriel.barrientos.c@uni.pe>
2023-05-28 11:21:36 -0500

    Sprint4 mejorado

---
+++ Sprint4.5/Sprint4/ReSOSgame/ReSOSgame/Controller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReSOSgame
+{
+    
+    public class Controller
+    {
+        private Tablero tablero;
+        public Tablero Tablero { get { return tablero; } set { tablero = value; } }
+        private Juego juego;
+        private Player player1, player2;
+        public Juego Juego { get {  return juego; } set { juego = value; } }
+        public Player Player1 { get { return player1; } set { player1 = value; } }
+        public Player Player2 { get { return player2; } set { player2 = value; } }
+        public Player CurrentPlayer { get { return Tablero.JugadorActual; } set { Tablero.JugadorActual = value; } }
+        public Tablero.Jugador Turno { get { return Tablero.Turno; } set { Tablero.Turno = value; } } 
+        public Tablero.Cell Ficha { get { return CurrentPlayer.Ficha; } set { CurrentPlayer.Ficha = value; } }
+        public int X { get { return CurrentPlayer.X; } }
+        public int Y { get { return CurrentPlayer.Y; } }
+        public int Tamanio { get { return Tablero.Tamanio; } }
+        public void InitTurn()
+        {
+            // Nota: Antes de usar el init turn debe haberse inicializado el player1
+            CurrentPlayer= player1;
+        }
+        public void ChangeTurn()
+        {
+            CurrentPlayer = (tablero.Turno == Tablero.Jugador.JAZUL) ? player1 : player2;
+        }
+    }
+}
---
+++ Sprint4.5/Sprint4/ReSOSgame/ReSOSgame/ScoreValidator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static ReSOSgame.Tablero;
+namespace ReSOSgame
+{
+    public class ScoreValidator
+    {
+        Tablero tablero;
+        public ScoreValidator(Tablero tablero)
+        {
+            this.tablero = tablero;
+        }
+        // Funcion booleana que verifica si el tablero esta lleno
+        public bool FullBoard()
+        {
+            for (int i = 0; i < tablero.Tamanio; i++)
+            {
+                for (int j = 0; j < tablero.Tamanio; j++)
+                {
+                    if (tablero.GetCell(i, j) == Tablero.Cell.VACIA)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        public bool HasOnePoint(int row, int col, Cell ficha)
+        {
+            if (ficha == Cell.S)
+            {
+                return (tablero.GetCell(row + 1, col + 1) == Cell.O && tablero.GetCell(row + 2, col + 2) == Cell.S) ||
+                    (tablero.GetCell(row - 1, col - 1) == Cell.O && tablero.GetCell(row - 2, col - 2) == Cell.S) ||
+                    (tablero.GetCell(row, col + 1) == Cell.O && tablero.GetCell(row, col + 2) == Cell.S) ||
+                    (tablero.GetCell(row, col - 1) == Cell.O && tablero.GetCell(row, col - 2) == Cell.S) ||
+                    (tablero.GetCell(row + 1, col) == Cell.O && tablero.GetCell(row + 2, col) == Cell.S) ||
+                    (tablero.GetCell(row - 1, col) == Cell.O && tablero.GetCell(row - 2, col) == Cell.S);
+            }
+            else if (ficha == Cell.O)
+            {
+                return (tablero.GetCell(row - 1, col - 1) == Cell.S && tablero.GetCell(row + 1, col + 1) == Cell.S) ||
+                    (tablero.GetCell(row + 1, col - 1) == Cell.S && tablero.GetCell(row - 1, col + 1) == Cell.S) ||
+                    (tablero.GetCell(row - 1, col) == Cell.S && tablero.GetCell(row + 1, col) == Cell.S) ||
+                    (tablero.GetCell(row, col - 1) == Cell.S && tablero.GetCell(row, col + 1) == Cell.S);
+            }
+            return false;
+        }
+        // Funcion que verifica si el juego se ha terminado
+        public bool GameOver()
+        {
+            return tablero.EstadoDeJuego != GameState.JUGANDO;
+        }
+        // Funcion que verifica si algunos de los dos jugadores ha ganado
+        public bool HasWon()
+        {
+            return tablero.EstadoDeJuego == Tablero.GameState.GANOAZUL ||
+                tablero.EstadoDeJuego == Tablero.GameState.GANOROJO;
+        }
+    }
+}
